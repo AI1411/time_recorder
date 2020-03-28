b7c9a76c8041e12fa7c64c5de35b8f9743197553
AI1411 <goethe0719@gmail.com>
2020-03-29 01:39:32 +0900

    small change

--- app/Exports/UsersExport.php
+++ app/Exports/UsersExport.php
@@ -5,8 +5,9 @@ namespace App\Exports;
 use App\Models\User;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithTitle;
 
-class UsersExport implements FromCollection, WithHeadings
+class UsersExport implements FromCollection, WithHeadings, WithTitle
 {
     /**
     * @return \Illuminate\Support\Collection
@@ -14,6 +15,11 @@ class UsersExport implements FromCollection, WithHeadings
     public function collection()
     {
         return User::all();
+    }
+
+    public function title():string
+    {
+        return 'test';
     }
 
     public function headings():array
