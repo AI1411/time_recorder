6144b982b9e4a65161855a78c770715b205790b7
AI1411 <goethe0719@gmail.com>
2020-03-31 10:16:25 +0900

    web.phpを編集。authでグループ化

--- routes/web.php
+++ routes/web.php
@@ -18,17 +18,21 @@ Auth::routes();
 Route::get('/', function () {
     return view('home');
 });
-Route::get('/users', 'UsersController@index')->name('users.index');
-Route::get('/users/{id}', 'UsersController@show')->name('users,show');
-Route::get('/userExport', 'UsersController@export')->name('users.export');
-
-Route::get('/stores/{id}', 'StoreController@show')->name('stores.show');
 
 
-Route::get('/attendances', 'AttendanceController@index')->name('attendances.index');
-Route::post('/attendances', 'AttendanceController@store')->name('attendances.store');
-
-
-Route::get('/atd_confirm', 'AtdConfirmController@index')->name('atd_confirm.index');
-
-Route::resource('expenses', 'ExpenseController')->only(['index', 'store']);
+Route::middleware(['auth'])->group(function () {
+    //従業員一覧
+    Route::get('/users', 'UsersController@index')->name('users.index');
+    Route::get('/users/{id}', 'UsersController@show')->name('users,show');
+    //従業員CSVエクスポート
+    Route::get('/userExport', 'UsersController@export')->name('users.export');
+    //店舗ページ
+    Route::get('/stores/{id}', 'StoreController@show')->name('stores.show');
+    //勤怠管理
+    Route::get('/attendances', 'AttendanceController@index')->name('attendances.index');
+    Route::post('/attendances', 'AttendanceController@store')->name('attendances.store');
+    //勤怠確認
+    Route::get('/atd_confirm', 'AtdConfirmController@index')->name('atd_confirm.index');
+    //経費申請
+    Route::resource('expenses', 'ExpenseController')->only(['index', 'store']);
+});
